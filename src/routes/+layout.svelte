a16d88d0844c88a0b0823164196d1c07665be625
Drew_Pfeiler <drewcpfeiler@gmail.com>
2023-08-13 11:33:31 -0500

    Adds hover vendor file + slight customization

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,16 @@
 <script>
     import '../styles/global.scss'
+    import '../styles/vendor/hover/hover.css'
 </script>
 
 <nav>
     <a class='logo' href="/">Drew Develops</a>
 
     <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/expertise">Expertise</a></li>
-        <li><a href="/previous-work">Previous Work</a></li>
-        <li><a href="/connect">Connect</a></li>
+        <li><a href="/" class="hvr-underline-from-center">Home</a></li>
+        <li><a href="/expertise" class="hvr-underline-from-center">Expertise</a></li>
+        <li><a href="/previous-work" class="hvr-underline-from-center">Previous Work</a></li>
+        <li><a href="/connect" class="hvr-underline-from-center">Connect</a></li>
     </ul>
 </nav>
 
@@ -37,22 +38,22 @@
             font-size: 1.2rem;
         }
 
-        .logo::after {
-            content: "";
-            width: 105px;
-            top: 70px;
-            left: 64px;
-            position: absolute;
-            border-bottom: 2px solid black;
-        }
     }
-
     ul {
         display: flex;
 
         li {
             list-style: none;
             margin-right: 1.3rem;
+
+            a {
+                padding-bottom: 4px;
+
+                &::before {
+                    background-color: grey !important;
+                    height: 2px !important;
+                }
+            }
         }
     }
 </style>
