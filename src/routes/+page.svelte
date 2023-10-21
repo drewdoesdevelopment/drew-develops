277c709e7b20c14574d643584445cf795ecea204
Drew_Pfeiler <drewcpfeiler@gmail.com>
2023-10-20 21:46:10 -0500

    Moar changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,10 @@
 <main>
     <div class="hero"> 
       <div class="flex-container">
-        <h1>Hey there, I'm Drew.</h1>
-        <h2>I'm a software engineer that focuses on creating <span>accessible</span> solutions.</h2>
-        <button>See My Work</button>
-        <button>Let's Connect</button>
+        <h1>Hey there, I'm Drew</h1>
+        <h2>I'm a software engineer that focuses on creating <span>accessible</span> solutions</h2>
+        <button class="primary">See My Work</button>
+        <button class="secondary">Let's Chat</button>
       </div>
       <div class="flex-container">
         <img src="/images/hero_svg.svg" />
@@ -20,8 +20,12 @@
     margin-top: 3rem;
   }
 
+  h1 {
+    font-weight: 600;
+  }
+
   h2 {
-    font-size: 1.2rem;
+    font-size: 1.1rem;
     font-weight: 500;
   }
 
@@ -29,5 +33,25 @@
     display: flex;
     align-items: center;
     justify-content: space-between;
+  }
+
+  button {
+    padding: 10px 26px;
+    border-color: transparent;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    font-weight: 500;
+    font-size: 0.8rem;
+
+    &.primary {
+      background-color: white;
+      border-radius: 5px;
+      margin-right: 0.5rem;
+    }
+
+    &.secondary {
+      background-color: black;
+      color: white;
+      border-radius: 47px;
+    }
   }
 </style>
