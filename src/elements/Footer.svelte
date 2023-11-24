e8ab45c10cc54288d77d2cf503fcd1a45fa5b9b3
Drew_Pfeiler <drewcpfeiler@gmail.com>
2023-11-24 08:49:57 -0600

    Adds in footer link svgs and necessary aria for accessibility

--- src/elements/Footer.svelte
+++ src/elements/Footer.svelte
@@ -1,13 +1,34 @@
-<script lang="ts">
-	console.log('footer is here');
-</script>
-
 <footer>
-    <p>Footer Text</p>
+    <div class="socials">
+        <h2>Follow me</h2>
+        <ul>
+            <li><a href="https://github.com/drewdoesdevelopment" target="_blank" rel="noreferrer"><img width="22" src="/images/github.svg" alt="Github brand icon" /></a></li>
+            <li><a href="https://github.com/drewdoesdevelopment" target="_blank" rel="noreferrer"><img width="22" src="/images/linkedin-in.svg" alt="Linkedin brand icon" /></a></li>
+        </ul>
+    </div>
 </footer>
 
-<style scoped>
-    footer {
-        margin: 0 4rem;
+<style lang="scss" scoped>
+	footer {
+		margin: 0 4rem;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	ul {
+		padding-left: 0;
+        list-style: none;
+        display: flex;
+        margin-left: 1rem;
+
+        li {
+            margin-right: 1rem;
+        }
+	}
+
+    .socials {
+        max-width: 200px;
     }
 </style>
