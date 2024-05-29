f2d83657b4b41b8cdbebcfa5ef49065e69e5d832
Drew Pfeiler <drew.pfeiler@xyleme.com>
2024-05-28 21:07:08 -0500

    Contact form progress

--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -2,33 +2,33 @@
 	<h1>Connect</h1>
 
 	<form action="submit">
-		<fieldset>
+		<fieldset class="contact">
             <legend>Contact Form</legend>
 			<label>
-				Name
+				<span>Name</span>
 				<input required type="text" />
 			</label>
 
 			<label>
-				Email
+				<span>Email</span>
 				<input required type="email" />
 			</label>
 
 			<label>
-				Phone (optional)
+				<span>Phone (optional)</span>
 				<input type="tel" />
 			</label>
 
-			<fieldset>
+			<fieldset class="commPreference">
 				<legend>Communication Preference</legend>
 
 				<label>
-					Phone
+					<span>Phone</span>
 					<input type="radio" name="commPreference" value="phone" />
 				</label>
 
 				<label>
-					Email
+					<span>Email</span>
 					<input type="radio" name="commPreference" value="email" />
 				</label>
 			</fieldset>
@@ -36,3 +36,56 @@
 	</form>
 	<button>Submit</button>
 </main>
+
+<style lang='scss'>
+form {
+    background-color: #18161c;
+    padding: 1rem;
+    border-radius: 10px;
+
+    label span {
+        padding-right: 10px;
+        border-right: 4px solid #6c63ff;
+        margin: 0.7rem 0;
+        width: max-content;
+    }
+
+    .contact { 
+        label {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+        }
+        
+        input {
+            background-color: #1f1d25;
+            border-color: transparent;
+            margin-left: 1rem;
+            height: 15px;
+        }
+    }
+
+    .commPreference {
+        padding-left: 0;
+
+        label {
+            flex-direction: row;
+        }
+
+        legend {
+            padding-top: 1rem;
+        }
+    }
+
+    legend {
+        text-align: center;
+        padding-left: 0;
+    }
+}
+
+fieldset {
+    display: flex;
+    flex-direction: column;
+    border: none;
+}
+</style>
