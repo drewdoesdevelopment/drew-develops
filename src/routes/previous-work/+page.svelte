3f13308b7980467d650b1e7ff2faef9326ffc829
Drew Pfeiler <drew.pfeiler@xyleme.com>
2024-06-21 19:15:33 -0500

    Finalizes previous-work mobile styles

--- src/routes/previous-work/+page.svelte
+++ src/routes/previous-work/+page.svelte
@@ -29,6 +29,8 @@ import projects from '../../data/workExamples.json';
 </main>
 
 <style lang='scss' scoped>
+@import '../../styles/variables.scss';
+
     .examples {
         display: flex;
         flex-direction: column;
@@ -70,6 +72,47 @@ import projects from '../../data/workExamples.json';
 
         .flex {
             margin-left: 3rem;
+        }
+    }
+
+
+    @media (max-width: $breakpoint-s) {
+        h1 {
+            margin-top: 3rem;
+            text-align: center;
+        }
+
+        h2 {
+            margin-top: 3rem;
+        }
+        .example, .example:nth-child(2n) {
+            flex-direction: column;
+
+        }
+        
+        .example:first-child {
+            margin-top: 3rem;
+        }
+
+        .example {
+            text-align: center;
+
+            &-details {
+                p {
+                    margin-top: 1rem;
+                    max-width: 100%;
+                }
+            }
+
+            .flex {
+                margin-left: 0;
+            }
+        }
+
+        .example:nth-child(2n) {
+            .flex {
+                margin-right: 0;
+            }
         }
     }
 </style>
